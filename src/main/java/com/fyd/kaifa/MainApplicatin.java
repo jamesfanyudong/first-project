5e1d977e00c73dbfd489273eab45da32a225808d
fyd <fyd2019@126.com>
2023-12-01 16:50:50 +0800

    項目初始化

---
+++ src/main/java/com/fyd/kaifa/MainApplicatin.java
@@ -0,0 +1,19 @@
+package com.fyd.kaifa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Auther: fyd20
+ * @Date: 2023/12/1 16:10
+ * @Description: MainApplicatin
+ * @Version 1.0.0
+ */
+@SpringBootApplication
+public class MainApplicatin {
+
+    public static void main(String[] args) {
+       SpringApplication.run(MainApplicatin.class);
+    }
+}
